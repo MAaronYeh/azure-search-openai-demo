51135beeb4329e4a727900bb2326a832b2312a82
Aaron Yeh <aayeh@microsoft.com>
2023-10-16 10:41:32 +0800

    Commit made from VS Code Azure Static Web Apps

---
+++ app/backend/static/assets/NoPage-a19a8c29.js
@@ -0,0 +1,2 @@
+import{bE as o}from"./vendor-d92a9189.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-a19a8c29.js.map
